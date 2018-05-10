6cd3d6161ecbaf573fdb8127d45204510d944e40
Adrian Wlodarczyk <adrian.wlodarczyk@ericsson.com>
2018-05-10 22:36:38 +0200

    test

---
+++ AndroidExample/Assets/TouchTest.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchTest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.touchCount > 0) {
+			//code
+			Touch tempTouch = Input.GetTouch(0);
+			//Debug.Log (tempTouch.position);
+		//foreach(Touch tempTouch in Input.touches){
+		//		Debug.Log(tempTouch.position);
+		//}
+			if (tempTouch.phase == TouchPhase.Began)
+				Debug.Log ("Nacisnieto");
+		}
+	}
+}
---
+++ AndroidExample/Assets/touchTest2.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class touchTest2 : MonoBehaviour {
+	Ray ray;
+	RaycastHit hit;
+	public GameObject touchSphereRange;
+	// Use this for initialization
+	void Start () {
+		//transform.position = touchSphereRange.transform.position - Vector3.forward * 10f;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.touchCount > 0) {
+			//ray = Camera.main.ScreenPointToRay (Input.GetTouch (0).position);
+
+			//if (Physics.Raycast (ray, out hit, Mathf.Infinity)) {
+				//Debug.Log ("Hit something");
+				//Destroy (hit.transform.gameObject);
+			Vector3 tempVector = new Vector3 (Camera.main.ScreenToWorldPoint (Input.GetTouch (0).position).x, 
+				                     Camera.main.ScreenToWorldPoint (Input.GetTouch (0).position).y,
+				                     touchSphereRange.transform.position.z);
+			touchSphereRange.transform.position = tempVector;
+			}
+		}
+}
