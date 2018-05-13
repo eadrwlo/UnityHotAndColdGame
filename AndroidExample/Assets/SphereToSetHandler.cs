058772fb7a3f72cb67a31c6aea7f761a6576fcdf
eadrwlo <wlodek14a@gmail.com>
2018-05-13 23:03:50 +0200

    Button for hiding and showing ball, Points label, background etc

---
+++ AndroidExample/Assets/SphereToSetHandler.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class SphereToSetHandler : MonoBehaviour {
+	public GameObject touchSphereRange;
+	int sphereBottomRenderMargin;
+	// Use this for initialization
+	void Start () {
+		//transform.position = touchSphereRange.transform.position - Vector3.forward * 10f;
+		int magicNumberHalfSphereHeight = 40;
+		sphereBottomRenderMargin = (int)Screen.height - (int)GameObject.FindGameObjectWithTag ("hideShowSpHereButton").GetComponent<RectTransform> ().rect.height - magicNumberHalfSphereHeight;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+		if ((Input.touchCount > 0) && (Input.GetTouch (0).position).y < sphereBottomRenderMargin) 
+		{
+			Vector3 tempVector = new Vector3 (Camera.main.ScreenToWorldPoint (Input.GetTouch (0).position).x, 
+				                     Camera.main.ScreenToWorldPoint (Input.GetTouch (0).position).y,
+				                     touchSphereRange.transform.position.z);
+			touchSphereRange.transform.position = tempVector;
+		}
+	}
+}
